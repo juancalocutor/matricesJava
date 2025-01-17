33b75043a3c3aa338ce73eaf1fe8089e52bd7fe6
juancalocutor <juancalocutor@gmail.com>
2025-01-16 20:00:15 -0500

    matrices buscar con etiqueta String

--- src/Matrices02_buscarEtiquetas.java
+++ src/Matrices02_buscarEtiquetas.java
@@ -14,6 +14,7 @@ public class Matrices02_buscarEtiquetas {
         System.out.print("\nY Cuántas columnas: ");
         columna = consola.nextInt();
         // CREAMOS LA MATRIZ
+        System.out.println();
         int[][] numeros = new int[fila][columna];
         // CICLOS PARA LLENAR LA MATRIZ
         for (int i = 0; i < fila; i++) {
@@ -40,10 +41,65 @@ public class Matrices02_buscarEtiquetas {
         }
 
         if (encontrado) {
-            System.out.println("\nNúmero " + perdido + " encontrado en la casilla [" + (i + 1) + "][" + (j + 1) + "]\n");
+            System.out
+                    .println("\nNúmero " + perdido + " encontrado en la casilla [" + (i + 1) + "][" + (j + 1) + "]\n");
         } else {
             System.out.println("El número no fue encontrado");
         }
 
+        System.out.println();
+        System.out.println("========================= AHORA CON STRING ======================\n");
+        // PEDIMOS DIMENSIONES DE LA MATRIZ DE STRING
+        System.out.print("¿Cuántas filas tendra la matriz? ");
+        int filaNombres = consola.nextInt();
+        System.out.println();
+        System.out.print("Y cuántas columnas ");
+        int columNombres = consola.nextInt();
+        consola.nextLine();
+        System.out.println();
+        // CREAMOS LA MATRIZ
+        String[][] nombreString = new String[filaNombres][columNombres];
+        // CICLO PARA PEDIR NOMBRES
+        for (int k = 0; k < filaNombres; k++) {
+            for (int k2 = 0; k2 < columNombres; k2++) {
+                System.out.print("Nombre " + (k + 1) + " - " + (k2 + 1) + ": ");
+                nombreString[k][k2] = consola.nextLine();
+            }
+        }
+
+        System.out.println();
+
+        // CILCO PARA IMPRIMIR NOMBRES
+        for (int k = 0; k < filaNombres; k++) {
+            for (int k2 = 0; k2 < columNombres; k2++) {
+                System.out.print("Nombre casilla [" + (k + 1) + "][" + (k2 + 1) + "]: " + nombreString[k][k2] + "\t");
+            }
+            System.out.println();
+        }
+
+        // CICLO PARA BUSCAR
+        System.out.println();
+        System.out.print("¿Qué nombre desea buscar? ");
+        String NN = consola.nextLine();
+
+        int k;
+        int k2 = 0;
+        boolean findNom = false;
+        Kraken: for (k = 0; k < filaNombres; k++) {
+            for (k2 = 0; k2 < columNombres; k2++) {
+                if (NN.equalsIgnoreCase(nombreString[k][k2])) {
+                    findNom = true;
+                    break Kraken;
+                }
+            }
+        }
+
+        System.out.println();
+
+        if (findNom) {
+            System.out.println("Nombre encontrado en la casilla [" + (k + 1) + "][" + (k2 + 1) + "]");
+        } else {
+            System.out.println("Nombre no encontrado");
+        }
     }
 }
